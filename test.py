818f75e82f2b68f0d7c5d01ff34256053e76bc80
hemjeet <hemjeetsingh88@gmail.com>
2023-10-07 21:35:31 +0530

    third

--- test.py
+++ test.py
@@ -7,3 +7,5 @@ from sklearn.datasets import make_classification
 X,y=make_classification(n_classes=2,n_samples=15000,n_features=5)
 
 y_pred=model.predict(X)
+with open('prediction.txt','w') as f:
+    f.write('Prediction successfulll ')
